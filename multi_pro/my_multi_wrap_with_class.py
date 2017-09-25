1cdc1abc37908d43d5e2a3f0f280ed9704bb0e75
nightimero <420739464@qq.com>
2017-09-25 23:02:11 +0800

    2017-9-25 23:02:02 提交

--- multi_pro/my_multi_wrap_with_class.py
+++ multi_pro/my_multi_wrap_with_class.py
@@ -4,11 +4,14 @@ import threading
 import time
 
 
-#todo: 用多线程运行类中的方法。
+#task：用多线程运行类中的方法。
 
-#todo： 用多线程运行函数。
-class my_thread():
-    def thread_run(urls):
+#task：用多线程运行函数。
+class my_thread(object):
+    def thread_run(self,urls):
+    # 为什么这里报错，但是奇怪的是，在aichat里面没有报错。
+    # 估计是args传递参数错了。晕死，竟然是类中的方法，忘写self了。。。 def thread_run(urls):
+    #def thread_run(urls):
         print 'Current %s is  running...' % threading.currentThread().name
         for url in urls:
             print "%s ---->>> %s" %(threading.currentThread().name,url)
@@ -19,10 +22,11 @@ print '%s is running ...' % threading.currentThread().name
 
 a = my_thread()
 b = my_thread()
-#todo: 为什么这里报错，但是奇怪的是，在aichat里面没有报错。
+
+#todo: 链接学习：https://stackoverflow.com/questions/25072409/how-to-pass-argument-into-threading
 t1 = threading.Thread(target=a.thread_run,name='Thread_1',args=(['url_1','url_2','url_3'],))
 t2 = threading.Thread(target=b.thread_run,name='Thread_2',args=(['url_4',"url_5",'url_6'],))
-
+t1.start()
 t2.start()
 t1.join()
 t2.join()
---
+++ my_design_pattern/my_singleton.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*-
+
+#第一种方式，python天然就是单例模式，在第一次导入时，pyc直接加载，不会再执行模块代码。
+# 所以需要把相关函数和数据定义在一个模块中，导入该模块就获得了一个单例类。
+
+class My_Singleton(object):
+    def foo(self):
+        pass
+
+my_singleton = My_Singleton()
+
+#其他文件中的调用代码
+from my_singleton import my_singleton
+my_singleton.foo()
+
+#第二种方法，使用 __new__和继承
+
+class Singleton(object):
+    _instances = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instances:
+            cls._instances = super(Singleton,cls).__new__(cls,*args,**kwargs)
+        return cls._instances
+
+class Myclass(Singleton):
+    a = 1
+
+#第三种方法，使用装饰器
+from functools import wraps
+
+#todo: 找到这个instance变量和其对于的字典，应该是co_freevar,学习co_开头变量的使用 ,没有看见一个dict变量类型的局部变量。
+def singleton(cls):
+    instance = {}
+    @wraps(cls)
+    def getinstance(*args,**kwargs):
+        if cls not in instance:
+            instance[cls] = cls(*args,**kwargs)
+        return instance[cls]
+    return getinstance
+
+#todo：类名称也可以作为字典的key？类名称是字符串？
+#todo：装饰器除了装饰费方法，还可以装饰类？ 搜索下。
+@singleton
+class MyClass(object):
+    a = 1
+#使用元类 http://funhacks.net/2017/01/17/singleton/
+#https://www.google.com/search?q=python%E5%8D%95%E4%BE%8B%E6%A8%A1%E5%BC%8F&oq=python%E5%8D%95%E4%BE%8B%E6%A8%A1%E5%BC%8F&aqs=chrome..69i57j0l5.12515j0j7&sourceid=chrome&ie=UTF-8
+# https://www.google.com/search?q=python+%E6%9F%A5%E7%9C%8B+%E6%96%B9%E6%B3%95+%E6%9C%AC%E5%9C%B0%E5%8F%98%E9%87%8F&oq=python+%E6%9F%A5%E7%9C%8B+%E6%96%B9%E6%B3%95+%E6%9C%AC%E5%9C%B0%E5%8F%98%E9%87%8F&aqs=chrome..69i57.22849j0j7&sourceid=chrome&ie=UTF-8
+
+
