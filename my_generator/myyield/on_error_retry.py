a3ded83b9bcffea804e8206a1ace118b59f65ecd
nightimero <chenxiang@aiknown.com>
2017-09-28 19:04:53 +0800

    2017-9-28 19:03:52 提交

--- my_generator/myyield/on_error_retry.py
+++ my_generator/myyield/on_error_retry.py
@@ -1,24 +1,24 @@
 # -*- coding:utf-8 -*-
 
 #=============================
-for i in range(0,100):
-    while True:
-        try:
-            # do stuff
-        except SomeSpecificException:
-            continue
-        break
-
-# ============================
-
-for i in range(100):
-  for attempt in range(10):
-    try:
-      # do thing
-    except:
-      # perhaps reconnect, etc.
-    else:
-      break
-  else:
-    # we failed all the attempts - deal with the consequences.
-
+# for i in range(0,100):
+#     while True:
+#         try:
+#             # do stuff
+#         except SomeSpecificException:
+#             continue
+#         break
+#
+# # ============================
+#
+# for i in range(100):
+#   for attempt in range(10):
+#     try:
+#       # do thing
+#     except:
+#       # perhaps reconnect, etc.
+#     else:
+#       break
+#   else:
+#     # we failed all the attempts - deal with the consequences.
+#
