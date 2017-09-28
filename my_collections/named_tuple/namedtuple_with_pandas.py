942e7035c44b7ef61aa105168b295528811e2342
nightimero <420739464@qq.com>
2017-09-28 23:03:08 +0800

    2017-9-13 22:45:28 提交

---
+++ my_collections/named_tuple/namedtuple_with_pandas.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+from collections import namedtuple
+import pandas as pd
+import numpy as np
+import threadpool
+
+Testdata = namedtuple("Testdata", ["first", "second", "third"])
+no1 = Testdata(1, 2, 3)
+
+def pp(data):
+    print data.first
+    print data.second
+
+dates = pd.date_range('20170928', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+# print df
+# todo: 熟悉logger，用不同的日志基本控制输出。
+no2 = Testdata(df,"test",123)
+# print no2
+# a = [no2]
+a = [no1]
+
+pool = threadpool.ThreadPool(2)
+# 这个函数就不支持namedtuple，需要字典。
+# result = request.callable(*request.args, **request.kwds)
+# TypeError: pp() argument after ** must be a mapping, not int
+requests = threadpool.makeRequests(pp,a)
+[pool.putRequest(req) for req in requests]
+pool.wait()
