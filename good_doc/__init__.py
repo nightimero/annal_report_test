8de77220eed2914c8627ac75106d4e67c43ed119
nightimero <chenxiang@aiknown.com>
2017-10-20 19:03:20 +0800

    模块内部自己获取自己的类。

--- good_doc/__init__.py
+++ good_doc/__init__.py
@@ -5,3 +5,7 @@
 # todo: http://kuanghy.github.io/2016/05/18/python-iteration
 # todo: http://wiki.jikexueyuan.com/project/the-python-study-notes-second-edition/
 
+http://blog.csdn.net/shijichao2/article/details/51165576
+https://ask.helplib.com/1299083
+http://www.jianshu.com/p/4c440ea08e00
+https://www.google.com/patents/CN1617102A?cl=zh
