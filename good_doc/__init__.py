265c68f6ebe0c7aa73fe22367ca3dfa4d6cdbf57
nightimero <420739464@qq.com>
2017-10-22 11:52:09 +0800

    2017-10-22 11:52:01

--- good_doc/__init__.py
+++ good_doc/__init__.py
@@ -1,11 +1,8 @@
 # -*- coding:utf-8 -*-
+
+# todo: http://blog.csdn.net/xiarendeniao/article/details/6774520
+# todo: http://wiki.jikexueyuan.com/project/the-python-study-notes-second-edition/
+# todo:https://www.google.com/patents/CN1617102A?cl=zh
 # todo: https://www.rddoc.com/doc/Python/3.6.0/zh/library/pkgutil/
 # todo: https://www.rddoc.com/
-# todo: http://blog.csdn.net/xiarendeniao/article/details/6774520
-# todo: http://kuanghy.github.io/2016/05/18/python-iteration
-# todo: http://wiki.jikexueyuan.com/project/the-python-study-notes-second-edition/
-
-http://blog.csdn.net/shijichao2/article/details/51165576
-https://ask.helplib.com/1299083
-http://www.jianshu.com/p/4c440ea08e00
-https://www.google.com/patents/CN1617102A?cl=zh
+# https://ask.helplib.com/1299083
