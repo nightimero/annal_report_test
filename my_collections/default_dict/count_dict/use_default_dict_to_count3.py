b2dbfd6a71e318d23115a049cf6198796a36ac3d
nightimero <chenxiang@aiknown.com>
2017-10-23 15:51:57 +0800

    2017-10-23 15:51:54

--- my_collections/default_dict/count_dict/use_default_dict_to_count3.py
+++ my_collections/default_dict/count_dict/use_default_dict_to_count3.py
@@ -24,12 +24,12 @@ class DefaultDict(dict):
     def __init__(self, default_factory=None, *a, **kw):
         dict.__init__(self, *a, **kw)
         self.default_factory = default_factory
-
-    def __getitem__(self, key):
-        try:
-            return dict.__getitem__(self, key)
-        except KeyError:
-            return self.__missing__(key)
+    # 这一段是没有用的。 __getitem__没有找到key，自然会掉__missing__方法。
+    # def __getitem__(self, key):
+    #     try:
+    #         return dict.__getitem__(self, key)
+    #     except KeyError:
+    #         return self.__missing__(key)
 
     def __missing__(self, key):
         self[key] = value = self.default_factory()
