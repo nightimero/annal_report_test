f9336b22f554b03a10cfa66000bd615e12f0d595
nightimero <chenxiang@aiknown.com>
2017-10-24 10:43:44 +0800

    调整文件结构

--- after_assert_run_other_case/my_after.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-
-class After(object):
-    def method_a(self):
-        print 'method_b'
-
-if __name__ == '__main__':
-    obj_after = After()
--- my_coroutine/async_pro.py
+++ my_coroutine/async_pro.py
@@ -5,7 +5,7 @@ Topic: 协程
 生产者-消费者实现
 """
 
-#https://www.xncoding.com/2015/12/21/python/nio.html
+# https://www.xncoding.com/2015/12/21/python/nio.html
 # 使用函数().next启动迭代器。
 # yield 前面是send入参，后面是返回值。
 # task： 在工作机器 添加 .ingnore 插件，使用文件右键ignore 功能。
@@ -14,25 +14,30 @@ Topic: 协程
 # todo：https://www.google.com/search?q=python+yield&oq=python+yield+&aqs=chrome..69i57.5194j0j7&sourceid=chrome&ie=UTF-8
 # todo： 深挖 yield 关键字
 import time
+
+
 def consumer():
     r = ''
     while True:
         n = yield r
-        #以下两句是可以不要的。
+        # 以下两句是可以不要的。
         # if not n:
         #     return
         print('[CONSUMER] Consuming %s...' % n)
         time.sleep(1)
         r = '200 OK'
-def producer(c):
-    c.next()
+        
+        
+def producer(consum):
+    consum.next()
     n = 0
     while n < 5:
         n += 1
         print('[PRODUCER] Producing %s...' % n)
-        r = c.send(n)
+        r = consum.send(n)
         print('[PRODUCER] Consumer return: %s' % r)
-    c.close()
+    consum.close()
+    
 if __name__ == '__main__':
     c = consumer()
     producer(c)
