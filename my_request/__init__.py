397c683fbe0a0d040c485843d06cef830a47fb9d
nightimero <chenxiang@aiknown.com>
2017-10-27 18:46:13 +0800

    requests模块

--- my_request/__init__.py
+++ my_request/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding:utf-8 -*-
 # todo: http://docs.python-requests.org/zh_CN/latest/user/quickstart.html
 # todo: http://docs.python-requests.org/zh_CN/latest/user/advanced.html
+# todo: you-get 是怎么请求和保存媒体文件的？
