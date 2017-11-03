de369b04aa933bdcfbbb5f004ed34a293c33f5b0
nightimero <chenxiang@aiknown.com>
2017-11-03 11:22:35 +0800

    2017-11-3

---
+++ my_time/remove_microsecond.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+import datetime
+now = datetime.datetime.now()
+print unicode(now.replace(microsecond=0))
+# 2011-11-03 11:19:07
+print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
---
+++ my_with/print_time_no_matter.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+import time
+
+
+class PrintTotalTime(object):
+    def __init__(self):
+        self.start_time = 0
+        self.end_time = 0
+
+    def __enter__(self):
+        self.start_time = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end_time = time.time()
+        print u'执行测试用例总共耗时{}秒'.format(self.end_time - self.start_time)
+
+
+with PrintTotalTime() as p:
+    time.sleep(10.5)
