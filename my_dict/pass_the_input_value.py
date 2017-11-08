80603c813b084e972643daf9e7ba9030db6aab6e
shawn <nightimero@163.com>
2017-11-08 23:56:21 +0800

    callback

--- my_dict/pass_the_input_value.py
+++ my_dict/pass_the_input_value.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+
 def print_b(**kwargs):
     print 'print_b', kwargs
     print 'print_b,a:', kwargs['a']
@@ -12,17 +13,9 @@ def print_a(**kwargs):
     print_b(**kwargs)
 
 
-
-
-
-
-
-
 dict_a = {'a': 1,
           'b': 2,
           'c': 3,
           }
-
 print_a(**dict_a)
-
 print_a(a=2, b=3, c=4)
