d50d7eeabb9984e6d9fbaf31bf961466111d80a2
nightimero <chenxiang@aiknown.com>
2017-11-08 17:27:37 +0800

    1. use **kwargs

--- my_dict/pass_the_input_value.py
+++ my_dict/pass_the_input_value.py
@@ -1,10 +1,22 @@
 # -*- coding:utf-8 -*-
 
+def print_b(**kwargs):
+    print 'print_b', kwargs
+    print 'print_b,a:', kwargs['a']
+
 
 def print_a(**kwargs):
     print kwargs['a']
     print kwargs['b']
     print kwargs['c']
+    print_b(**kwargs)
+
+
+
+
+
+
+
 
 dict_a = {'a': 1,
           'b': 2,
