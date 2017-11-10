367009e0a96e2775abf5f7ad8a68b07803caa884
nightimero <chenxiang@aiknown.com>
2017-11-10 19:39:04 +0800

    1. pool and thread

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 # -*- coding:utf-8 -*-
+# TODO: https://github.com/Voidly/Parallel-Programming-with-Python
+# TODO: https://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/001386832973658c780d8bfa4c6406f83b2b3097aed5df6000
+# TODO: http://cuyu.github.io/python/2016/09/05/Python%E4%B8%AD%E7%9A%84%E4%BF%A1%E5%8F%B7%E6%9C%BA%E5%88%B6%E5%88%9D%E7%AA%A5
+# TODO: http://www.developerq.com/article/1507550819
+# TODO: https://stackoverflow.com/questions/12990199/how-to-fetch-process-thread-name-levelname-in-customized-python-logger
+# TODO: https://www.ibm.com/developerworks/cn/opensource/os-cn-pythonwith/index.html
