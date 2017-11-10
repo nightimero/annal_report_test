2afa6371c5e962338ef3567fb87f37a56033b9af
nightimero <chenxiang@aiknown.com>
2017-11-10 16:44:42 +0800

    1. lock the queue

---
+++ my_multi_run/ThreadLocal.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+import threading
+
+# 创建全局ThreadLocal对象:
+local_school = threading.local()
+
+def process_student():
+    print 'Hello, %s (in %s)' % (local_school.student, threading.current_thread().name)
+
+def process_thread(name):
+    # 绑定ThreadLocal的student:
+    local_school.student = name
+    process_student()
+
+t1 = threading.Thread(target= process_thread, args=('Alice',), name='Thread-A')
+t2 = threading.Thread(target= process_thread, args=('Bob',), name='Thread-B')
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+# 在多线程环境下，每个线程都有自己的数据。一个线程使用自己的局部变量比使用全局变量好，
+# 因为局部变量只有线程自己能看见，不会影响其他线程，而全局变量的修改必须加锁。
+#
+# 但是局部变量也有问题，就是在函数调用的时候，传递起来很麻烦
+
+def process_student(name):
+    std = Student(name)
+    # std是局部变量，但是每个函数都要用它，因此必须传进去：
+    do_task_1(std)
+    do_task_2(std)
+
+def do_task_1(std):
+    do_subtask_1(std)
+    do_subtask_2(std)
+
+def do_task_2(std):
+    do_subtask_2(std)
+    do_subtask_2(std)
