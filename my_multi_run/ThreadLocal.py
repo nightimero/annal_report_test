e559de472725c7f509981e7ded93d0be661a70d4
nightimero <chenxiang@aiknown.com>
2017-11-10 19:34:34 +0800

    1. pool and thread

--- my_multi_run/ThreadLocal.py
+++ my_multi_run/ThreadLocal.py
@@ -12,8 +12,8 @@ def process_thread(name):
     local_school.student = name
     process_student()
 
-t1 = threading.Thread(target= process_thread, args=('Alice',), name='Thread-A')
-t2 = threading.Thread(target= process_thread, args=('Bob',), name='Thread-B')
+t1 = threading.Thread(target=process_thread, args=('Alice',), name='Thread-A')
+t2 = threading.Thread(target=process_thread, args=('Bob',), name='Thread-B')
 t1.start()
 t2.start()
 t1.join()
