16d3c8f005458289a4683dd31d737fa970193dd8
nightimero <chenxiang@aiknown.com>
2017-11-15 18:18:46 +0800

    1. charde

--- my_selenium/chrome_headless.py
+++ my_selenium/chrome_headless.py
@@ -13,3 +13,9 @@ driver = webdriver.Chrome(chrome_options=options)
 driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/sbin/chromedriver')  # linux 需要指定
 driver.get('http://www.baidu.com')
 print driver.title
+
+
+# 以前遇到过driver.get(url)一直不返回，但也不报错的问题，这时程序会卡住，设置超时选项能解决这个问题。
+# driver.set_page_load_timeout(20)
+# 设置10秒脚本超时时间
+# driver.set_script_timeout(20)
