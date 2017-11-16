e9840aa8056c996c6fa6deb4e424055b9b737a34
nightimero <chenxiang@aiknown.com>
2017-11-16 16:40:33 +0800

    1. pycallgraph

--- my_itchat/login_webchat.py
+++ my_itchat/login_webchat.py
@@ -1,3 +1,15 @@
 import itchat
-itchat.auto_login()
-print 'test'
+import time
+from pycallgraph import PyCallGraph
+from pycallgraph.output import GraphvizOutput
+
+graphviz = GraphvizOutput()
+graphviz.output_file = ('out2.png')
+
+with PyCallGraph(output=graphviz):
+    itchat.auto_login()
+    time.sleep(3)
+    itchat.logout()
+
+# todo: why out.png and out2.png cannot show the itchat module.
+
---
+++ my_trace/my_call_graph/commond_line.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# python C:\Python27\Scripts\pycallgraph graphviz -- prod_test\product_chat.py
