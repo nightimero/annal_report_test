4cb8a630b508d980081114ed42ada3356df9f89f
nightimero <chenxiang@aiknown.com>
2017-11-16 16:41:50 +0800

    1. pycallgraph

--- my_trace/my_call_graph/commond_line.py
+++ my_trace/my_call_graph/commond_line.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 # python C:\Python27\Scripts\pycallgraph graphviz -- prod_test\product_chat.py
+# http://pycallgraph.slowchop.com/en/master/guide/command_line_usage.html#general-arguments
