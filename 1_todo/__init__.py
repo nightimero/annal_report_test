9beff0e7b5b9b4a114224d9d57b88ed9728fefd7
nightimero <420739464@qq.com>
2017-12-04 22:51:31 +0800

    1

--- 1_todo/__init__.py
+++ 1_todo/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
+# todo: 2017-12-4  https://huilansame.github.io/huilansame.github.io/archivers/python-unittest
+# todo: 2017-12-4  http://pyunit.sourceforge.net/pyunit_cn.html
 # todo: http://www.jackyshen.com/2015/05/21/async-operations-in-form-of-sync-programming-with-python-yielding/
