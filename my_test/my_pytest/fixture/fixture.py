29993193c84a6570d9c7f93843f2f4c1b5cfd25c
shawn <nightimero@163.com>
2018-01-23 23:40:21 +0800

    parametrize

--- my_test/my_pytest/fixture/fixture.py
+++ my_test/my_pytest/fixture/fixture.py
@@ -8,20 +8,28 @@ def before():
     return "abc"
 
 
+# def test1(before()): 这样是错误的。
 def test1(before):
     print 'func test1'
 
 
-def test2(before):
+def test2():
+    # before 这样不行
+    before()
     print 'func test2'
 
+# 两种方法使用fixture。
+# 一、在函数或方法开始加注解。
+# 二、在函数或方法内部调用。
 class Testhaha(object):
-    def test3(before):
-        print before
+    # @pytest.mark.usefixtures("before")
+    def test3(self):
+        print before()
         print 'func test3'
 
-    def test4(before):
+    def test4(self):
         print 'func test4'
+
 
 if __name__ == "__main__":
     pytest.main("fixture.py -s -v")
