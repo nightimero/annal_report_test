69e6792a9fe0e6f201af6c664a4c0e620759b60c
nightimero <chenxiang@aiknown.com>
2018-01-24 10:51:36 +0800

    fixture

---
+++ my_test/my_pytest/fixture/test_app.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+class App(object):
+    a = 1
+    b = 2
+
+@pytest.fixture(scope='module')
+def app():
+    return App()
+
+def test_smtp_exists(app):
+    print app.a
+    app.a = 3
+    print app.b
+    app.b = 4
+    assert app.a
+
+def test_smtp_exists2(app):
+    print app.a
+    print app.b
+    assert app.a
